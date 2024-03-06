eeb06607ffeed0d032fbdf8eb5961a1949229204
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-06 23:53:44 +0600

    Mobile hunting base api implement done

--- phone_hunter api/js/index.js
+++ phone_hunter api/js/index.js
@@ -1,1 +1,33 @@
-console.log("phone hunter")
+function loadPhon() {
+    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+        .then(res => res.json())
+        .then(res => displayPhone(res.data))
+}
+
+
+function displayPhone(phones) {
+    //step-1 
+    const phonecontainer=document.getElementById('phone-container')
+    phones.forEach(phone => {
+        
+        //step-2 create div
+        const phonecard = document.createElement('div')
+        phonecard.classList = `card w-96 bg-base-100 shadow-xl`
+         //step-3 create innerhtml
+        phonecard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+                    <div class="card-body">
+                      <h2 class="card-title">${phone.phone_name}</h2>
+                      <p>${phone.slug}</p>
+                      <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                      </div>
+                    </div>
+        
+        `;
+         //step-4 append
+        phonecontainer.appendChild(phonecard)
+
+    });
+}
+loadPhon()
